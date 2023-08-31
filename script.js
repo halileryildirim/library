1d71ee8b07b756acbfa06363dd7420b8621413bb
halileryildirim <halileryildirim@gmail.com>
2023-08-31 20:50:23 +0300

    Removed required tags since confirmBtn is not default. fixed read:on bug

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const myLibrary = [];
 function Book(title, author, pages, read) {
     this.title = title,
     this.author = author,
-    this.pages = pages
+    this.pages = pages,
     this.read = read
 }
 
@@ -11,7 +11,7 @@ function addBookToLibrary() {
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
     let pages = document.querySelector("#pages").value;
-    let read = document.querySelector("#read").value;
+    let read = document.querySelector("#read").checked;
 
     const newBook = new Book(title,author,pages,read);
     myLibrary.push(newBook);
